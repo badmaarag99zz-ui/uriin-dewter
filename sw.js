1b5899fd6effe852b092f329a73e5985f30791e1
badmaarag99zz-ui <badmaarag99zz@gmail.com>
2026-09-17 17:40:30 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "uriin-dewter-v4";
+const CACHE_NAME = "uriin-dewter-v6";
 
 const FILES_TO_CACHE = [
     "/",
@@ -6,7 +6,6 @@ const FILES_TO_CACHE = [
     "/manifest.json"
 ];
 
-// Install
 self.addEventListener("install", event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
@@ -15,7 +14,6 @@ self.addEventListener("install", event => {
     );
 });
 
-// Activate
 self.addEventListener("activate", event => {
     event.waitUntil(
         caches.keys()
@@ -30,41 +28,37 @@ self.addEventListener("activate", event => {
     );
 });
 
-// Fetch
 self.addEventListener("fetch", event => {
     if (event.request.method !== "GET") {
         return;
     }
 
     event.respondWith(
-        caches.match(event.request)
-            .then(cachedResponse => {
-                if (cachedResponse) {
-                    return cachedResponse;
+        fetch(event.request)
+            .then(response => {
+
+                if (
+                    response &&
+                    response.status === 200
+                ) {
+                    const clone = response.clone();
+
+                    caches.open(CACHE_NAME)
+                        .then(cache => {
+                            cache.put(
+                                event.request,
+                                clone
+                            );
+                        });
                 }
 
-                return fetch(event.request)
-                    .then(networkResponse => {
-                        if (
-                            networkResponse &&
-                            networkResponse.status === 200
-                        ) {
-                            const responseClone = networkResponse.clone();
-
-                            caches.open(CACHE_NAME)
-                                .then(cache => {
-                                    cache.put(
-                                        event.request,
-                                        responseClone
-                                    );
-                                });
-                        }
-
-                        return networkResponse;
-                    });
+                return response;
             })
             .catch(() => {
-                return caches.match("/index.html");
+                return caches.match(event.request)
+                    .then(cached => {
+                        return cached || caches.match("/index.html");
+                    });
             })
     );
 });
